4ee31bdf605dbaea2e992c7f49bf34f85fa19f9f
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-04-10 00:36:49 +0300

    Added Caesars cipher encryptor

---
+++ src/ciphers/caesar.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const fs = require('fs');
+
+const encryptByCaesar = (file, step = 1) => {
+  const data = (fs.readFileSync(file, 'utf-8')).split('');
+  const encrypted = [];
+
+  for (const char of data) {
+    let encryptedChar = char.toLowerCase();
+    let code = encryptedChar.codePointAt();
+
+    if (encryptedChar.match(/[a-z]/)) {
+      code = (((code - 'a'.codePointAt() + step) % 26) + 'a'.codePointAt());
+      encryptedChar = String.fromCodePoint(code);
+    }
+
+    if (char.match(/[A-Z]/)) {
+      encryptedChar = encryptedChar.toUpperCase();
+    }
+
+    encrypted.push(encryptedChar);
+  }
+
+  fs.writeFileSync('encrypted.txt', encrypted.join(''));
+};
+
+module.exports = { encryptByCaesar };
