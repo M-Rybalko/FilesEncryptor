3615f840d7a4dfd35fbf7c059c486a789663922a
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-04-13 18:18:39 +0300

    Improved Morse encrypting function

--- src/ciphers/morse.js
+++ src/ciphers/morse.js
@@ -6,18 +6,18 @@ const morseAlphabet = require('./morseAlphabet');
 const encryptByMorse = (file) => {
   const data = (fs.readFileSync(file, 'utf-8')).toUpperCase().split('');
   const encrypted = [];
-  const dividers = ['.', ',', ' ', ';', ':', '"', '?', '!',
-    '[', ']', '{', '}', '(', ')', '&', '—'];
 
   for (let i = 0; i < data.length; i++) {
     let char = data[i];
-    const nextChar = (data[i + 1]);
+    const nextChar = data[i + 1];
+    const keys = Object.keys(morseAlphabet);
 
-    if (Object.keys(morseAlphabet).includes(char)) {
+    if (keys.includes(char)) {
       char = morseAlphabet[char];
-      if (Object.keys(morseAlphabet).includes(nextChar)) char += '   ';
-      if (dividers.includes(nextChar)) char += '       ';
+      if (keys.includes(nextChar)) char += '   ';
     }
+
+    if (char === ' ' && keys.includes(nextChar)) char = '       ';
 
     encrypted.push(char);
     fs.writeFileSync('encrypted.txt', encrypted.join(''));
