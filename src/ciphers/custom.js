ef228db12b3e9034fc7cd8d99a152d483fbe805f
M-Rybalko <maxymrybalko2005@gmail.com>
2023-04-17 23:23:22 +0300

    Improved styling

--- src/ciphers/custom.js
+++ src/ciphers/custom.js
@@ -12,6 +12,7 @@ const rl = readline.createInterface({
 const createCipher = async () => {
   const cipher = {};
   let repeat = true;
+
   while (repeat) {
     const toReplace = await rl.question('Enter anything you need to replace: ');
     const replacer = await rl.question('Enter a substitution string: ');
@@ -21,21 +22,26 @@ const createCipher = async () => {
     if (stop === 'y') repeat = false;
     if (stop === 'n') repeat = true;
   }
+
   rl.close();
   return cipher;
 };
 
 const encryptByCustom = (file, cipher) => {
+
   const data = fs.readFileSync(file, 'utf-8');
   let encrypted = data;
+
   if (data === '') {
     console.log('File is empty. Try choosing another file');
     rl.close();
     return;
   }
+
   for (const key of Object.keys(cipher)) {
     encrypted = encrypted.replaceAll(key, cipher[key]);
   }
+
   fs.writeFileSync('encrypted.txt', encrypted);
 };
 
