c1808026698ee6cc7191b4e063b25ea6d3b5382c
M-Rybalko <maxymrybalko2005@gmail.com>
2023-04-21 22:14:37 +0300

    Added command tiers raw version

--- src/commands.js
+++ src/commands.js
@@ -15,38 +15,49 @@ const rl = readline.createInterface({
 rl.prompt();
 
 const commands = {
-  help() {
-    const commandList = fs.readFileSync('./docs/commandList.txt', 'utf-8');
-    console.log(commandList);
+
+  general: {
+
+    help() {
+      const commandList = fs.readFileSync('./docs/commandList.txt', 'utf-8');
+      console.log(commandList);
+    },
+
+    exit() {
+      rl.close();
+    },
+
+    async encrypt() {
+      const answer = await rl.question('Which file do you want to encrypt? ');
+      const file = './' + answer;
+      const cipher = await rl.question('Which cipher do you want to use? ');
+      if (cipher === 'morse') morse.encryptByMorse(file);
+      if (cipher === 'caesar') {
+        const step = await rl.question('How many hops do you need? ');
+        caesar.encryptByCaesar(file, step);
+      }
+    },
   },
 
-  exit() {
-    rl.close();
-  },
+  custom: {
 
-  async encrypt() {
-    const answer = await rl.question('Which file do you want to encrypt? ');
-    const file = './' + answer;
-    const cipher = await rl.question('Which cipher do you want to use? ');
-    if (cipher === 'morse') morse.encryptByMorse(file);
-    if (cipher === 'caesar') {
-      const step = await rl.question('How many hops do you need? ');
-      caesar.encryptByCaesar(file, step);
-    }
-  },
-
-  async create() {
-    console.log(`Welcome to cipher creating! 
-      Here you can create, customise and save any ciphers!
-      Type cipherHelp to see all commands:`);
-  },
+    create() {
+      console.log(`Welcome to cipher creating! 
+        Here you can create, customise and save any ciphers!
+        Type cipherHelp to see all commands:`);
+    },
+  }
 
 };
 
 rl.on('line', (line) => {
   line = line.trim();
-  const command = commands[line];
-  if (command) command();
-  else console.log('Unknown command. Type "help" to see available commands');
+  const [tier, name] = line.split(' ');
+  const level = commands[tier];
+  if (level) {
+    const command = level[name];
+    if (command) command();
+    else console.log('Unknown command. Type "help" to see available commands');
+  } else console.log('This level does not exist');
   rl.prompt();
 }).on('close', () => process.exit(0));
