2d90940efa23cb0f1f3daf21f34df0206230a535
M-Rybalko <maxymrybalko2005@gmail.com>
2023-05-04 23:01:39 +0300

    Major changes in several functions

--- src/classes/CustomCipher.js
+++ src/classes/CustomCipher.js
@@ -53,29 +53,27 @@ class CustomCipher {
   }
 
   omitChar(char) {
-    const omit = this.cipher.omit;
 
     if (char === '') {
       console.log('You cant omit an empty line!');
       return;
     }
 
-    omit.add(char);
+    this.omit.add(char);
     return this.cipher;
   }
 
   deomit(char) { // Change name
-    const omit = this.cipher.omit;
     char = char.trim();
 
-    if (omit.has(char)) {
-      omit.delete(char);
+    if (this.omit.has(char)) {
+      this.omit.delete(char);
 
-      if (!omit.has(char)) {
+      if (!this.omit.has(char)) {
         console.log(char + ' will not be omited since now');
       }
 
-      return omit;
+      return this.omit;
     }
 
     console.log('This character is not in omit list');
@@ -84,9 +82,11 @@ class CustomCipher {
 
   deleteCipher(name) {
     if (name === this.name) {
-      this.cipher = {};
-      if (Object.keys(this.cipher).length === 0) {
-        console.log(this.name + ' was deleted successfuly!');
+      delete this.cipher;
+      delete this.omit;
+      delete this.name;
+      if (!this.cipher && !this.omit && !this.name) {
+        console.log('Cipher was deleted successfuly!');
       }
       return;
     }
@@ -95,19 +95,13 @@ class CustomCipher {
 
   showCipher(name) { // Remake to errors
     const ommitedChars = [];
-    const omit = this.cipher.omit;
 
     if (name !== this.name) {
       console.log('The cipher with this name doesnt exist.');
       return;
     }
 
-    if (Object.keys(this.cipher).length === 0) {
-      console.log('The cipher with this name was deleted.');
-      return;
-    }
-
-    if (Object.keys(this.cipher).length === 1 && omit.size === 0) {
+    if (Object.keys(this.cipher).length === 0 && this.omit.size === 0) {
       console.log('This cipher is empty.');
       return;
     }
@@ -120,32 +114,41 @@ class CustomCipher {
       }
     }
 
-    for (const char of omit) {
+    for (const char of this.omit) {
       ommitedChars.push(`"${char}"`);
     }
 
     console.log('List of ommited characters: ' + ommitedChars.join(', '));
   }
 
-  saveToJSON(cipherName, fileName) {
+  saveToJSON(cipherName) {
 
     if (cipherName !== this.name) {
       console.log('The cipher with this name doesnt exist.');
       return;
     }
 
-    if (Object.keys(this.cipher).length === 0) {
-      console.log('The cipher with this name was deleted.');
-      return;
+    const GENERAL_FOLDER = '../Custom ciphers';
+    if (!fs.existsSync(GENERAL_FOLDER)) fs.mkdirSync(GENERAL_FOLDER);
+    const CIPHER_FOLDER = '../Custom cipher/' + this.name;
+    if (!fs.existsSync(CIPHER_FOLDER)) fs.mkdirSync(CIPHER_FOLDER);
+
+    const cipher = JSON.stringify(this.cipher, null, 2);
+    const omitArray = [];
+
+    for (const char of this.omit) {
+      omitArray.push(char);
     }
 
-    const FOLDER = '../Custom ciphers';
-    const data = JSON.stringify(this.cipher, null, 2);
-    const file = FOLDER + '/' + fileName + '.json';
+    const omit = { omit: omitArray };
 
-    if (!fs.existsSync(FOLDER)) fs.mkdirSync(FOLDER);
-    fs.writeFileSync(file, data);
-    if (fs.existsSync(file)) {
+    const cipherFile = CIPHER_FOLDER + '/cipher.json';
+    fs.writeFileSync(cipherFile, cipher);
+
+    const omitFile = CIPHER_FOLDER + '/omit.json';
+    fs.writeFileSync(omitFile, JSON.stringify(omit, null, 2));
+
+    if (fs.existsSync(cipherFile) && fs.existsSync(omitFile)) {
       console.log('Saved successfuly!');
     }
   }
